b1f7b05721b2d057bcfdd79942210812af6e942c
Gerald Scharitzer <gerald.scharitzer@gmail.com>
2022-01-09 10:18:23 +0100

    factors

--- unitsofmeasure/test/test_unit.py
+++ unitsofmeasure/test/test_unit.py
@@ -1,29 +1,33 @@
 """Test Unit"""
 import pytest
+from fractions import Fraction
 from unitsofmeasure import Dimension, no_prefix, no_unit, Prefix, scalar, Unit
 from unitsofmeasure.decprefix import DecimalPrefixes
 
-# TODO test fraction
+_one = Fraction(1,1)
+
 @pytest.mark.parametrize(
-    "symbol , name       , dimension        , prefix"          ,[
-    ("%"    , "percent"  , Dimension()      , no_prefix        ), # scalar
-    ("kg"   , "kilogram" , Dimension(kg=1)  , DecimalPrefixes.k), # SI base units
-    ("m"    , "metre"    , Dimension(m=1)   , no_prefix        ),
-    ("s"    , "second"   , Dimension(s=1)   , no_prefix        ),
-    ("A"    , "ampere"   , Dimension(A=1)   , no_prefix        ),
-    ("K"    , "kelvin"   , Dimension(K=1)   , no_prefix        ),
-    ("cd"   , "candela"  , Dimension(cd=1)  , no_prefix        ),
-    ("mol"  , "mole"     , Dimension(mol=1) , no_prefix        )
+    "symbol , name       , dimension        , prefix,           , factor",[
+    ("%"    , "percent"  , Dimension()      , no_prefix         , Fraction(1,100)), # scalar
+    ("kg"   , "kilogram" , Dimension(kg=1)  , DecimalPrefixes.k , _one), # SI base units
+    ("m"    , "metre"    , Dimension(m=1)   , no_prefix         , _one),
+    ("s"    , "second"   , Dimension(s=1)   , no_prefix         , _one),
+    ("A"    , "ampere"   , Dimension(A=1)   , no_prefix         , _one),
+    ("K"    , "kelvin"   , Dimension(K=1)   , no_prefix         , _one),
+    ("cd"   , "candela"  , Dimension(cd=1)  , no_prefix         , _one),
+    ("mol"  , "mole"     , Dimension(mol=1) , no_prefix         , _one)
 ])
-def test_unit(symbol: str, name: str, dimension: Dimension, prefix: Prefix) -> None:
-    unit = Unit(symbol=symbol, name=name, dimension=dimension, prefix=prefix)
+def test_unit(symbol: str, name: str, dimension: Dimension, prefix: Prefix, factor: Fraction) -> None:
+    unit = Unit(symbol, name, dimension, prefix, factor)
     assert unit.symbol    == symbol
     assert unit.name      == name
     assert unit.dimension == dimension
     assert unit.prefix    == prefix
+    assert unit.factor    == factor
 
 def test_no_unit() -> None:
     assert no_unit.symbol    == ""
     assert no_unit.name      == ""
     assert no_unit.dimension == scalar
     assert no_unit.prefix    == no_prefix
+    assert no_unit.factor    == _one
