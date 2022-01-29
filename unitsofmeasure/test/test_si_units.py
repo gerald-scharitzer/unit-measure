c3676f45c7566a96eebd214574afc36a6fa3e67a
Gerald Scharitzer <gerald.scharitzer@gmail.com>
2022-01-29 18:45:13 +0100

    accepted units

--- unitsofmeasure/test/test_si_units.py
+++ unitsofmeasure/test/test_si_units.py
@@ -1,8 +1,8 @@
 """Test SI Units"""
-from unitsofmeasure import base, derived
+from unitsofmeasure import accepted, base, derived
 
 def test() -> None:
-    units = base.si_base_units | derived.si_derived_units
+    units = base.si_base_units | derived.si_derived_units | accepted.si_accepted_units
     for (key, unit) in units.items():
         print(key, unit, unit.name)
         if (key == "degC"):
