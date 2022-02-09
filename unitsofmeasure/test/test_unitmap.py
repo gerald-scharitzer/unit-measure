77961d9688e6aade4cef9b64ed1338fd7e148014
Gerald Scharitzer <gerald.scharitzer@gmail.com>
2022-02-09 20:43:57 +0100

    rc3

--- unitsofmeasure/test/test_unitmap.py
+++ unitsofmeasure/test/test_unitmap.py
@@ -1,5 +1,5 @@
 """Test UnitMap"""
-from unitsofmeasure import get_unit, map_to_unit, Unit, UnitMap
+from unitsofmeasure import get_unit, map_to_unit, set_unit, Unit, UnitMap
 
 def test() -> None:
     # not all objects are weakly referencable, but class instances are
@@ -23,3 +23,13 @@ def test_decorator() -> None:
 
     unit = get_unit(func)
     assert unit == b
+
+def test_set_unit() -> None:
+    class Measure:
+        def __init__(self, value: object) -> None:
+            self.value = value
+    measure = Measure(10)
+    B = Unit("B", "byte")
+    set_unit(measure, B)
+    unit = get_unit(measure)
+    assert unit == B
