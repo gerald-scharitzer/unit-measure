7793dfab4846a1bec1f4d686f26e249a4d30d4e0
Gerald Scharitzer <gerald.scharitzer@gmail.com>
2022-06-04 11:25:47 +0200

    derived quantities

--- unitsofmeasure/test/test_derived.py
+++ unitsofmeasure/test/test_derived.py
@@ -5,7 +5,7 @@ def test():
     assert len(items) == 22 # there are 22 derived units
 
     for (key, unit) in items:
-        print(key, unit, unit.name)
+        print(key, unit, unit.name, unit.dimension.symbol, unit.dimension.name)
 
         if (key == "degC"):
             assert unit.symbol == "°C"
@@ -14,5 +14,12 @@ def test():
         
         assert len(unit.symbol) > 0
         assert len(unit.name) > 0
+
+        if (key == "kat"):
+            assert len(unit.dimension.symbol) == 0
+        else:
+            assert len(unit.dimension.symbol) > 0
+        
+        assert len(unit.dimension.name) > 0
         assert unit.prefix == PREFIX_1
         assert unit.factor == Unit.FRACTION_1
