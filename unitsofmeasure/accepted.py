ac4e39ea602b553c32d152d697c7b1b8611477af
Gerald Scharitzer <gerald.scharitzer@gmail.com>
2022-06-26 16:12:49 +0200

    dimension symbol and name

--- unitsofmeasure/accepted.py
+++ unitsofmeasure/accepted.py
@@ -7,8 +7,8 @@ h   = Unit("h", "hour", base.s.dimension, factor=Fraction(3600,1)) # time: 60 mi
 d   = Unit("d", "day", base.s.dimension, factor=Fraction(86_400,1)) # time: 24 hours
 au  = Unit("au", "astronomical unit", base.m.dimension, factor=Fraction(149_597_870_700,1)) # length: distance between Earth and Sun
 # angular degree, minute, and second are not implemented
-ha  = Unit("ha", "hectare", Dimension(m=2), factor=Fraction(10_000,1)) # area
-l   = Unit("l", "litre", Dimension(m=3), factor=Fraction(1,1000)) # volume
+ha  = Unit("ha", "hectare", Dimension(m=2, symbol="A", name="area"), factor=Fraction(10_000,1)) # area
+l   = Unit("l", "litre", Dimension(m=3, symbol="V", name="volume"), factor=Fraction(1,1000)) # volume
 t   = Unit("t", "tonne", base.kg.dimension, factor=Fraction(1000,1)) # mass
 # dalton, electronvolt, neper, bel, and decibel are not implemented
 
